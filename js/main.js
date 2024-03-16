7768c6bcebed379dd0faa1060198f1eb948f2671
Diego Solis <Endgame189@outlook.com>
2024-03-16 16:18:34 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,27 +1,56 @@
+const apiKey = 'e49ae78c30d6e842a2841907db176af6'; // OpenWeather API key
+
 function dateTime() {
-	const date = new Date();
-	let today = date.toDateString();
-	let time = date.toLocaleTimeString();
-	document.getElementById('date-time').innerHTML = '<p id="date">' + today + '</p><p id="time">' + time + '</p>';
-	setTimeout(dateTime, 1000);
+    const date = new Date();
+    let today = date.toDateString();
+    let time = date.toLocaleTimeString();
+    document.getElementById('date-time').innerHTML = `<p id="date">${today}</p><p id="time">${time}</p>`;
+    setTimeout(dateTime, 1000);
 }
 
 function weatherBalloon(cityID) {
-	var apiKey = ''; //OpenWeather API key
-	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + apiKey)
-		.then(function(resp) {
-			return resp.json()
-		})
-		.then(function(data) {
-			let weatherIcon = data.weather[0].icon;
-			let tempK = parseFloat(data.main.temp);
-			let tempC = Math.round(tempK - 273.15);
-			let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
-			document.getElementById('weather').innerHTML = '<p id="location">' + data.name + '</p><p id="details" ' + 'title="' + tempF + '&deg;F">' + '<img src="https://openweathermap.org/img/wn/' + weatherIcon + '.png">' + data.weather[0].description + '<span class="separator">|</span>' + tempC + '&deg;C</p>';
-		});
+    fetch(`https://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${apiKey}&units=imperial`)
+        .then(function(resp) {
+            if (!resp.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return resp.json();
+        })
+        .then(function(data) {
+            console.log('API Response:', data);
+            let weatherIcon = data.weather[0].icon;
+            let tempF = Math.round(data.main.temp); // Temperature in Fahrenheit
+            document.getElementById('weather').innerHTML = `<p id="location">${data.name}</p><p id="details" title="${tempF}&deg;F"><img src="https://openweathermap.org/img/wn/${weatherIcon}.png">${data.weather[0].description}<span class="separator">|</span>${tempF}&deg;F</p>`;
+        })
+        .catch(function(error) {
+            console.error('Error fetching weather data:', error);
+        });
 }
 
 function traichu() {
-	dateTime();
-	weatherBalloon(1850147); //OpenWeather city ID
+    if (!document.getElementById('date-time')) {
+        console.error("Element with ID 'date-time' not found.");
+        return;
+    }
+
+    dateTime();
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`)
+                .then(function(resp) {
+                    return resp.json();
+                })
+                .then(function(data) {
+                    let weatherIcon = data.weather[0].icon;
+                    let tempF = Math.round(data.main.temp);
+                    document.getElementById('weather').innerHTML = `<p id="location">${data.name}</p><p id="details" title="${tempF}&deg;F"><img src="https://openweathermap.org/img/wn/${weatherIcon}.png">${data.weather[0].description}<span class="separator">|</span>${tempF}&deg;F</p>`;
+                });
+        });
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+        weatherBalloon(1850147); // Default location
+    }
 }
+
+traichu();
